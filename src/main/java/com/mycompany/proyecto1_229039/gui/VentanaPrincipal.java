df9031c24df1679b5c958c4ba958270ec7022a96
AlejandroMontijo <alexmonty68@gmail.com>
2026-02-10 18:54:29 -0700

    feat: VentanaPrincipal - ventana principal con JTabbedPane y barra de estado

---
+++ src/main/java/com/mycompany/proyecto1_229039/gui/VentanaPrincipal.java
@@ -0,0 +1,54 @@
+package com.mycompany.proyecto1_229039.gui;
+
+import com.mycompany.proyecto1_229039.persistencia.PersistenciaFachada;
+
+public class VentanaPrincipal extends javax.swing.JFrame {
+
+    private PersistenciaFachada fachada;
+
+    public VentanaPrincipal() {
+        fachada = new PersistenciaFachada();
+        initComponents();
+        setSize(900, 650);
+
+        // agregar pestañas con paneles personalizados
+        tabbedPane.addTab("Pacientes", new PanelPacientes(fachada));
+        tabbedPane.addTab("Médicos", new PanelMedicos(fachada));
+        tabbedPane.addTab("Inventario", new PanelInventario(fachada));
+        tabbedPane.addTab("Consultas", new PanelConsultas(fachada));
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        tabbedPane = new javax.swing.JTabbedPane();
+        panelEstado = new javax.swing.JPanel();
+        lblEstado = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Sistema de Gestión Hospitalaria");
+        setMinimumSize(new java.awt.Dimension(800, 550));
+        getContentPane().setLayout(new java.awt.BorderLayout());
+
+        tabbedPane.setFont(new java.awt.Font("SansSerif", 0, 13));
+        getContentPane().add(tabbedPane, java.awt.BorderLayout.CENTER);
+
+        panelEstado.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        panelEstado.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+
+        lblEstado.setText("Sistema Hospitalario v1.0 - Proyecto 1");
+        panelEstado.add(lblEstado);
+
+        getContentPane().add(panelEstado, java.awt.BorderLayout.PAGE_END);
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lblEstado;
+    private javax.swing.JPanel panelEstado;
+    private javax.swing.JTabbedPane tabbedPane;
+    // End of variables declaration//GEN-END:variables
+}
